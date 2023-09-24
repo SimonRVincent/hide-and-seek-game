22afdf937e2720bb0233059a77359747f54a1d7f
Simon Vincent <47257674+SmonVincent@users.noreply.github.com>
2023-09-24 14:08:56 -0600

    Added base hider features.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
       {currentScreen === 'menu' && <MainMenu setScreen={setCurrentScreen} />}
       {currentScreen === 'choice' && <ChoiceScreen setScreen={setCurrentScreen} />}
       {currentScreen === 'lobby' && <LobbyScreen setScreen={setCurrentScreen} />}
+      {currentScreen === 'hider' && <HiderScreen setScreen={setCurrentScreen} />}
       {/* Add other screens here similarly */}
     </div>
   );
@@ -50,10 +51,33 @@ function LobbyScreen({ setScreen }) {
 
 function HiderScreen({ setScreen }) {
   // Here, you'd implement the score increasing logic and other functionalities for the Hider
+  const [score, setScore] = useState(0);
+  const [isFound, setIsFound] = useState(false);
+
+  useEffect(() => {
+    if (isFound) return;  // If the hider is found, don't increase the score
+
+    const interval = setInterval(() => {
+      setScore(prevScore => prevScore + 1);
+    }, 1000);  // Increase score by 1 every second
+
+    return () => clearInterval(interval);  // Cleanup on component unmount
+  }, [isFound]);
+
   return (
-    <div>
-      Hider Screen
-      {/* Placeholder for score and other UI */}
+    <div onClick={() => !isFound && setIsFound(true)}>
+      {isFound ? (
+        <div>
+          <h2>You've been found!</h2>
+          <p>Score: {score}</p>
+          <button onClick={() => setScreen('menu')}>Return to Main Menu</button>
+        </div>
+      ) : (
+        <div>
+          <h2>Hide!</h2>
+          <p>Score: {score}</p>
+        </div>
+      )}
     </div>
   );
 }
